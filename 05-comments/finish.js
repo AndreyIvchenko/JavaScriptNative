9f7d865a35f094875f1c04c3648bb3164f753de2
AndreyIvchenko <6491666@gmail.com>
2022-08-16 20:03:43 +0300

    add tasks on reposit

---
+++ 05-comments/finish.js
@@ -0,0 +1,22 @@
+/** ЗАДАЧА 5 - Комментарии
+ *
+ * 1. Добавьте однострочный комментарий
+ *
+ * 2. Объявите любую переменную и опишите ее
+ * с помощью комментария в той же строке
+ *
+ * 3. Создайте многострочный комментарий
+ */
+
+// my  comment for JS
+
+const valueUserText = ''; //this variable for will get users text 
+
+/* option 1 : comment string 1 
+string 2
+string 3 */
+
+/** option 2 : comment line first
+ * commen line second
+ * comment line third
+ */
---
+++ 06-data-types/finish.js
@@ -0,0 +1,40 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+const myName = 'Andrei';
+console.log(typeof myName)
+
+const age = 28;
+console.log(typeof age)
+
+const lightOn = true;
+console.log(typeof lightOn)
+
+const container = null;
+console.log(typeof container)
+
+const spirit = undefined;
+console.log(typeof spirit)
+
+const person = {
+    name: 'Ann',
+    male: 'woman',
+    age: 44
+}
+console.log(typeof person)
+
+const array = [1,2,3];
+console.log(typeof array)
+
